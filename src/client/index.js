85482733911dd3cdaec6e05e7103eb0a55d05e76
Ivan Olmo <ivan.olmo@gmail.com>
2020-08-17 16:44:52 -0500

    Add function to retrieve coordinates of city input by user

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,10 @@
 import './styles/resets.scss';
 import './styles/main.scss';
 import './styles/footer.scss';
+import "@babel/polyfill";
+import { geonames } from './js/geonames';
+import { getLocation, getWeather, getImage } from "./js/helpers";
+
+document.getElementById('submit-add-trip').addEventListener('click', geonames);
+
+export { getLocation, getWeather, getImage };
